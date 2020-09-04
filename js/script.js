30928bc0cbf4557e7f7b5d59603530e5c64bcde2
valentina-x <xzxz100.95@mail.ru>
2020-09-04 14:58:43 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+    var leftBorderWidth = 1;
+    let second = 2;
+    const pi = 3.14; 
+};
+
+// let answer = prompt("Are you here?", "yes");
+// console.log(answer);
+// if(answer==null) console.log('otmena');
+
